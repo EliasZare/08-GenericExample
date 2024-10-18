8aa2a999862676971e11ed667ec573612df135aa
EliasZare <1EliasZare1@gmail.com>
2024-10-18 22:43:25 +0330

    Add project files.

---
+++ 08-GenericExample/GenricClass.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _08_GenericExample
+{
+    public class GenricClass<T, TKey> where T : class where TKey : Test
+    {
+        public T Salary { get; set; }
+        public T MyProperty { get; set; }
+    }
+
+
+    public static class GenricMethods
+    {
+        public static void GenricMethod<T>(T obj) where T : Test
+        {
+            Console.WriteLine(obj.MyProperty);
+        }
+    }
+}
+    
+    
---
+++ 08-GenericExample/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+using _08_GenericExample;
+
+Console.WriteLine("Hello, World!");
+
+//Genric Class
+GenricClass<string, Test> genricClassInt = new GenricClass<string, Test>();
+GenricClass<string, Test> genricClassStr  = new GenricClass<string, Test>();
+
+
+//Generic Methods
+Test test = new Test(myProperty:"1011");
+GenricMethods.GenricMethod<Test>(test);
+
+genricClassInt.Salary = "100";
+genricClassStr.Salary = "Hundred";
+Console.WriteLine(genricClassInt.Salary);
+Console.WriteLine(genricClassStr.Salary);
+Console.ReadKey();
